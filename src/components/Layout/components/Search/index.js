4065b2f3723521be767442528ebb716acf8a0464
ANHNX4 <anh833905@gmail.com>
2022-05-19 15:35:52 +0700

    logic Header search delay request Api

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -8,6 +8,8 @@ import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
 import styles from './Search.module.scss';
 
+import { useDebounce } from '~/hooks';
+
 const cx = classNames.bind(styles);
 
 function Search() {
@@ -16,16 +18,18 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -34,7 +38,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');
@@ -45,7 +49,6 @@ function Search() {
     const handleHideResult = () => {
         setShowResult(false);
     };
-
     return (
         <HeadlessTippy
             interactive
